4de669f5bda82bf6c120e85e9679e45d920ab7ab
jorgetrajano <jorgetrajano1997@gmail.com>
2022-04-13 02:38:41 -0400

    Portifólio inicial

---
+++ blog-pessoal.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Blog do Jorge</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+			
+		<section>
+			<header>
+				<img src="jorgeperfil.jpg" alt="Foto de perfil de Jorge Trajano" class="perfil">
+				<h1 id="title">Jorge Trajano</h1>
+				<h4 class="subtitle">Developer FullStack</h4>
+			</header>
+
+			<article class="post">
+				<header>
+					<h5 class="sobre-mim">Sobre mim:</h5>
+				</header>
+					
+				<p>
+				Estudante de Engenharia de Petróleo e Gás - UFAM. <a href="https://www.linkedin.com/in/jorge-trajano-73a574185/" target="_blank">Jorge</a> possui conhecimento em gestão da Semana acadêmica de Engenharia de Petróleo e Gás, gerir a Comissão Eleitoral do Centro Acadêmico. E ainda, experiência na Regulagem, Controle e Fiscalização das obras públicas concedidas do Estado do Amazonas.
+				
+				Encontrei um na área de Tecnologia uma nova luz, sempre buscando aprender mais sobre linguagem de programação e desenvolvimento fullstack.
+				</p>
+			</article>
+
+			<article class="projetos">
+				<p>
+					<img src="background.jpg" alt="Imagem de terminal do desenvolvedor" class="fotoback">
+					Além deste portifólio, os demais projetos estão disponíveis no <a href="https://github.com/jorgetrajano/" target="_blank">GitHub.</a> Para mais informações, entrar em contato clicando em um dos links abaixo:
+				</p>
+
+			</article>
+		
+		</section>
+		
+		<footer>
+			<ul class="contatos">
+				<li>
+					<h6><a href="https://instagram.com/jorgetrajano12" target="_blank">@jorgetrajano12</a></h6>
+				</li>
+				<li>
+					<h6><a href="telto: +55 92 994976242" target="_blank">92 99497-6242</a></h6>
+				</li>
+				<li>
+					<h6>
+						<a href="https://www.linkedin.com/in/jorge-trajano-73a574185/" target="_blank">Linkedin</a>
+					</h6>
+				</li>
+				<li>
+					<h6><a href="https://github.com/jorgetrajano/">GitHub</a></h6>
+				</li>
+			</ul>
+		</footer>
+	</body>
+</html>
